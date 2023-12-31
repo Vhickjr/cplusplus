6673329e729ccc2377cf7f639e94c3cd2e4c0701
vhickjr <victormathias001@gmail.com>
2023-12-31 11:52:32 +0100

    new commit

---
+++ sum_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+double num1;
+double num2;
+ cout<<"enter the first number you want to add"<<endl;
+ cin>>num1;
+ cout<<"enter the second number you want to add"<<endl;
+ cin>>num2;
+double sum = num1 + num2;
+cout<<"the sum is " <<sum<<endl;
+ system("pause");
+return 0;
+}
